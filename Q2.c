9dfd4e67da0ef7af7c3148fd89466707dc9eac4b
Muhammad Ameen <i190690@nu.edu.pk>
2023-05-10 12:36:31 +0500

    Second commit, added comments to the code

--- Q2.c
+++ Q2.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 int FRecursive(int n) { //this is a recursive function 
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    if (n == 2) return 2;
+    if (n == 0){
+        return 0;
+    }
+    else if (n == 1){
+        return 1;
+    }
+    else if (n == 2){
+        return 2;
+    }
     return FRecursive(n - 3) + FRecursive(n - 2);
 }
 int FIterative(int n) {
-    if (n == 0) return 0; //this function follows the iterative method in a loop to solve the problem
-    if (n == 1) return 1;
-    if (n == 2) return 2;
+    if (n == 0){
+        return 0;
+    } //this function follows the iterative method in a loop to solve the problem
+    else if (n == 1){
+        return 1;
+    }
+    else if (n == 2){
+        return 2;
+    }
 
     int a = 0, b = 1, c = 2, i;
     for (i = 3; i <= n; i++) {
@@ -21,9 +33,15 @@ int FIterative(int n) {
 }
 
 int FMemo(int n) { //this method follows memoization technique, store the result in a array so that we don't have to compute it again.
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    if (n == 2) return 2;
+    if (n == 0){
+        return 0;
+    }
+    else if (n == 1){
+        return 1;
+    }
+    else if (n == 2){
+        return 2;
+    }
 
     int memo[n+1];
     memo[0] = 0;
